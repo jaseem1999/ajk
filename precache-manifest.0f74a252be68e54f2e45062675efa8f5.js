1641095f6264f3a260991c9eab5c432834782641
Adil <ajkkalappatt@gmail.com>
2021-12-07 17:08:10 +0300

    Updates

---
+++ precache-manifest.0f74a252be68e54f2e45062675efa8f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ce77346776913dc876919052f6b378",
+    "url": "/ajk/index.html"
+  },
+  {
+    "revision": "279176fd2ab8013e9a60",
+    "url": "/ajk/static/css/2.fbf08e0d.chunk.css"
+  },
+  {
+    "revision": "fb839635bf0cdcc09bea",
+    "url": "/ajk/static/css/main.01278d50.chunk.css"
+  },
+  {
+    "revision": "279176fd2ab8013e9a60",
+    "url": "/ajk/static/js/2.24fbe3ec.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ajk/static/js/2.24fbe3ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb839635bf0cdcc09bea",
+    "url": "/ajk/static/js/main.cd351645.chunk.js"
+  },
+  {
+    "revision": "cc2abc75b09b3f9dd79c",
+    "url": "/ajk/static/js/runtime-main.aebf9923.js"
+  },
+  {
+    "revision": "28378a1fa1ca62e075ae4b47db5b3d91",
+    "url": "/ajk/static/media/me.28378a1f.jpg"
+  }
+]);
