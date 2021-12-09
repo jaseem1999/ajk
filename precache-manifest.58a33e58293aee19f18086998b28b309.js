2d0b0e0e58b05abee5dbc48d118d84578abd551a
Adil <ajkkalappatt@gmail.com>
2021-12-09 22:45:51 +0300

    Updates

---
+++ precache-manifest.58a33e58293aee19f18086998b28b309.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb8d77f9a2ca4b650fa4499010243c3",
+    "url": "/ajk/index.html"
+  },
+  {
+    "revision": "2dbd70969b6fda8da952",
+    "url": "/ajk/static/css/2.fbf08e0d.chunk.css"
+  },
+  {
+    "revision": "a0a5cffcd7e436a3191f",
+    "url": "/ajk/static/css/main.85bf0f5a.chunk.css"
+  },
+  {
+    "revision": "2dbd70969b6fda8da952",
+    "url": "/ajk/static/js/2.cbe5e52f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ajk/static/js/2.cbe5e52f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a5cffcd7e436a3191f",
+    "url": "/ajk/static/js/main.ce91560c.chunk.js"
+  },
+  {
+    "revision": "cc2abc75b09b3f9dd79c",
+    "url": "/ajk/static/js/runtime-main.aebf9923.js"
+  },
+  {
+    "revision": "e9db3c757e36bc82383c797fbf2c4f1d",
+    "url": "/ajk/static/media/AINIT.e9db3c75.png"
+  },
+  {
+    "revision": "47e0a086ffa67fb97c3e9c7c86b89219",
+    "url": "/ajk/static/media/HNIT.47e0a086.png"
+  },
+  {
+    "revision": "8c8465689bc1ad1a59eae7ac10a207b0",
+    "url": "/ajk/static/media/hedge.8c846568.png"
+  },
+  {
+    "revision": "2899c700812f4f460571df799ce95f75",
+    "url": "/ajk/static/media/john.2899c700.png"
+  },
+  {
+    "revision": "28378a1fa1ca62e075ae4b47db5b3d91",
+    "url": "/ajk/static/media/me.28378a1f.jpg"
+  },
+  {
+    "revision": "014fddba1fea9a39a7ef4f8b28a82cc8",
+    "url": "/ajk/static/media/python.014fddba.png"
+  },
+  {
+    "revision": "111b6a65002c28073fc800964d80b50d",
+    "url": "/ajk/static/media/robo.111b6a65.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ajk/precache-manifest.37b4ad21af3b5705a550f6c892ba46e6.js"
+  "/ajk/precache-manifest.58a33e58293aee19f18086998b28b309.js"
 );
 
 self.addEventListener('message', (event) => {
